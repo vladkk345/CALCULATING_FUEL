3a46274c17e86ef5752bda5767d0858eec45650b
Владислав Кононович <55635774+vladkk345@users.noreply.github.com>
2021-10-03 18:21:23 +0300

    Add files via upload

---
+++ Prog2.cpp
@@ -0,0 +1,40 @@
+﻿// Prog2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <stdio.h>
+#include <Windows.h>
+#include "Header.h"
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int mode;
+	printf("Введите 0 для метрического режима или 1 для американского режима: ");
+	scanf_s("%d", &mode);
+
+	while (mode >= 0)
+	{
+		set_mode(mode);
+		get_info();
+		show_info();
+		printf("Введите 0 для метрического режима иди 1 для американского режима");
+		printf("(-1 для завершения): ");
+		scanf_s("%d", &mode);
+
+		if (mode > 1)
+		{
+			set_mode(mode);
+			get_info();
+			show_info();
+			printf("Введите 0 для метрического режима иди 1 для американского режима");
+			printf("(-1 для завершения): ");
+			scanf_s("%d", &mode);
+		}		
+	}
+
+	printf("Программа завершена.\n");
+	return 0;
+}
+
